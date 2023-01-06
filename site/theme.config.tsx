3f6861b36dda58b81c1dc6ba86f7b267f2624153
Duke <duc@silentium.io>
2023-01-06 05:35:29 +0000

    feat: added site

---
+++ site/theme.config.tsx
@@ -0,0 +1,9 @@
+import { DocsThemeConfig } from "nextra-theme-docs/.";
+
+export default {
+  logo: <span>Submodule documentation</span>,
+  project: {
+    link: 'https://github.com/silenteer/submodule',
+  },
+  // ...
+} satisfies DocsThemeConfig
