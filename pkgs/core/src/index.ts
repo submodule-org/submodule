ea6e1cbed692a67e8c506d553aa55fa0df72291e
Duke <duc@silentium.io>
2023-06-19 06:15:19 +0000

    feat: added ProvideOption to all operations

--- pkgs/core/src/index.ts
+++ pkgs/core/src/index.ts
@@ -20,9 +20,12 @@ export function setInstrument(inst: CreateInstrumentHandler) {
 }
 
 export type Executor<Provide, Dependent = unknown> = {
-  execute<Output>(executable: Provider<Output, Provide>): Promise<Output>
+  execute<Output>(executable: Provider<Output, Provide>, options?: ProviderOption): Promise<Output>
   get(): Promise<Provide>
-  prepare<Input extends Array<any>, Output>(provider: (provide: Provide, ...input: Input) => Output): (...input: Input) => Promise<Awaited<ReturnType<typeof provider>>>
+  prepare<Input extends Array<any>, Output>(
+    provider: (provide: Provide, ...input: Input) => Output,
+    options?: ProviderOption
+  ): (...input: Input) => Promise<Awaited<ReturnType<typeof provider>>>
   _inject: (executor: Executor<Dependent>) => void
 }
 
@@ -30,21 +33,27 @@ function isExecutor<P>(obj: any): obj is Executor<P> {
   return typeof obj?.['get'] === 'function'
 }
 
-export type Hijacked<Dependent> = (executor: Executor<Dependent>) => void
+function extract<Dependent>(
+  secondParam?: ProviderOption | Executor<Dependent>,
+  thirdParam?: ProviderOption
+) {
+  return isExecutor(secondParam)
+    ? { dependent: secondParam, options: thirdParam }
+    : { dependent: undefined, options: secondParam }
+}
+
+function set(target: any, name: string, value: any) {
+  Object.defineProperty(target, name, {value: value, writable: false});
+}
 
 export function create<Provide>(provider: Provider<Provide>, options?: ProviderOption): Executor<Provide>
-export function create<Provide, Dependent>(provider: Provider<Provide>, dependent?: Executor<Dependent>, options?: ProviderOption): Executor<Provide>
+export function create<Provide, Dependent>(provider: Provider<Provide, Dependent>, dependent?: Executor<Dependent>, options?: ProviderOption): Executor<Provide>
 export function create<Provide, Dependent = unknown>(
   provider: Provider<Provide, Dependent>,
   secondParam?: ProviderOption | Executor<Dependent>,
   thirdParam?: ProviderOption,
-): Executor<Provide> {
-  const dependent = isExecutor(secondParam)
-    ? secondParam
-    : undefined
-
-  const options = thirdParam
-
+): Executor<Provide, Dependent> {
+  const { dependent, options } = extract(secondParam, thirdParam)
   const opts = { ...defaultProviderOptions, ...options }
 
   const name = opts?.name || provider.name || 'anonymous'
@@ -73,41 +82,22 @@ export function create<Provide, Dependent = unknown>(
     return cached
   }
 
-  async function execute<Output>(provider: Provider<Output, Provide>): Promise<Output> {
-    return provider(await get())
+  const executor: Executor<Provide, Dependent> = { 
+    execute: async (provider, options) => create(async () => provider(await executor.get()), options || { name: `${name}.execute` }).get(),
+    get: async() => (await init()).provide,
+    prepare(provider, options) {
+      return async function () {
+        const that = this
+        const args = arguments
+        return execute(async () => provider.call(that, await executor.get(), ...args), options)
+      }
+    },
+    _inject: async (executor) => { dependentRef = executor }
   }
 
-  async function get(): Promise<Provide> {
-    const { provide } = await init()
-    return provide
-  }
-
-  function prepare<Input extends Array<any>, Output>(provider: (provide: Provide, ...input: Input) => Output): (...input: Input) => Promise<Awaited<ReturnType<typeof provider>>> {
-    return async function () {
-      const that = this
-      const args = arguments
-      return value(provider).execute(async fn => fn.call(that, await get(), ...args))
-    }
-  }
-
-  const _inject: Hijacked<Dependent> = async (executor) => { dependentRef = executor }
-
-  const executor = { execute, get, prepare, _inject }
-
-  Object.defineProperty(executor.get, 'name', {
-    value: `${name}.get`,
-    writable: false
-  });
-
-  Object.defineProperty(executor.execute, 'name', {
-    value: `${name}.execute`,
-    writable: false
-  });
-
-  Object.defineProperty(executor.prepare, 'name', {
-    value: `${name}.prepare`,
-    writable: false
-  });
+  set(executor.get, 'name', `${name}.get`)
+  set(executor.execute, 'name', `${name}.execute`)
+  set(executor.prepare, 'name', `${name}.prepare`)
 
   if (opts.instrument) {
     instrument(executor, opts.instrument)
@@ -116,16 +106,17 @@ export function create<Provide, Dependent = unknown>(
   return executor
 }
 
-export const value = <Provide>(value: Provide) => create(() => value)
+export const value = <Provide>(value: Provide, options?: Omit<ProviderOption, 'mode'>) => create(() => value, options)
 
 export const prepare = <Dependent, Input, Output>(provider: (provide: Dependent, input: Input) => Output, dep: Executor<Dependent>): (input: Input) => Promise<Awaited<ReturnType<typeof provider>>> => dep.prepare(provider)
 
-export async function execute<Output>(executable: Provider<Output>): Promise<Output>
-export async function execute<Output, Dependent>(executable: Provider<Output, Dependent>, dependent: Executor<Dependent>): Promise<Output>
-export async function execute<Dependent, Output>(executable: Provider<Output, Dependent>, dependent?: Executor<Dependent>): Promise<Output> {
+export async function execute<Output>(executable: Provider<Output>, options?: ProviderOption): Promise<Output>
+export async function execute<Output, Dependent>(executable: Provider<Output, Dependent>, dependent: Executor<Dependent>, options?: ProviderOption): Promise<Output>
+export async function execute<Dependent, Output>(executable: Provider<Output, Dependent>, secondParam?: Executor<Dependent> | ProviderOption, thirdParam?: ProviderOption): Promise<Output> {
+  const { dependent, options } = extract(secondParam, thirdParam)
   return dependent
-    ? dependent.execute(executable)
-    : create(executable).get()
+    ? dependent.execute(executable, options)
+    : create(executable, options).get()
 }
 
 export type inferProvide<T> = T extends Executor<infer S> ? S : never
