18882dac8e31f9dc96dbd3ee7f09eb728b13acce
Duke <duc@silentium.io>
2024-02-22 05:27:43 +0000

    feat: added proxify package

---
+++ pkgs/proxify/index.ts
@@ -0,0 +1,34 @@
+const supportedType = ['object', 'array']
+
+export const proxify = <T>(value: T) => {
+  const proxied = {}
+
+  if (!supportedType.includes(typeof value)) {
+    return {
+      value,
+      update: () => { throw new Error('not supported') }
+    }
+  }
+
+  let ref: object = value as any
+
+  return {
+    value: new Proxy(proxied, {
+      apply(_, thisArgs, argumentList) {
+        const refFn = ref as unknown as () => any
+
+        return refFn.apply(thisArgs, argumentList)
+      },
+      get(_, prop, __) {
+        const value = Reflect.get(ref, prop)
+        return proxify(value).value
+      },
+      set(_, prop, value, __) {
+        return Reflect.set(ref, prop, value)
+      }
+    }) as typeof value,
+    update: (newValue: T) => {
+      ref = newValue as any
+    }
+  }
+}
