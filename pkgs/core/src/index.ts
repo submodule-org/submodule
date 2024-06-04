1937024b65ed7a77edbbca7fce33f5531360b74b
Duke <duc@silentium.io>
2024-06-04 11:23:18 +0700

    feat: added scope and its api

--- pkgs/core/src/index.ts
+++ pkgs/core/src/index.ts
@@ -2,7 +2,7 @@ class ProviderClass<Provide, Input = unknown> {
   constructor(
     private provider: (scope: Scope, input: Input) => Provide | Promise<Provide>,
     private dependencies?: Executor<Input, unknown>
-  ) {}
+  ) { }
 
   async run(scope: Scope): Promise<Provide> {
     const input = this.dependencies ? scope.resolve(this.dependencies) : undefined
@@ -13,7 +13,7 @@ class ProviderClass<Provide, Input = unknown> {
 type Provider<Provide, Input = unknown> = (input: Input) => Provide | Promise<Provide>
 
 export class Scope {
-  constructor(private store = new Map<Executor<any, any>, Promise<any>>()) {}
+  constructor(private store = new Map<Executor<any, any>, Promise<any>>()) { }
 
   has(executor: Executor<any, any>) {
     return this.store.has(executor)
@@ -23,8 +23,13 @@ export class Scope {
     return this.store.get(executor)
   }
 
-  async set(executor: Executor<any, any>, value: any) {
-    this.store.set(executor, value)
+  async set<T>(executor: Executor<T, unknown>, v: T | Promise<T> | Executor<T, unknown>) {
+    if (isExecutor(v)) {
+      this.store.set(executor, v.resolve(this))
+      return
+    }
+
+    this.store.set(executor, Promise.resolve(v))
   }
 
   resolveValue<T>(executor: Executor<T, any>, value: T | Executor<T, any>) {
@@ -45,7 +50,7 @@ export class Scope {
   }
 
   async execute<Dependent, Output>(
-    executable: Provider<Output, Dependent>, 
+    executable: Provider<Output, Dependent>,
     dependency: Executor<Dependent, unknown>
   ): Promise<Awaited<Output>> {
     const value = await this.resolve(dependency)
@@ -55,12 +60,17 @@ export class Scope {
   prepare<Dependent, Input extends Array<any>, Output>(
     provider: (provide: Dependent, ...input: Input) => Output,
     dependency: Executor<Dependent, unknown>,
-  ): (...input: Input) => Promise<Awaited<ReturnType<typeof provider>>>  {
+  ): (...input: Input) => Promise<Awaited<ReturnType<typeof provider>>> {
+    const scope = this
     return async function () {
       const that = this
       const args = arguments
-      return execute(async (d) => provider.call(that, d, ...args), dependency)
+      return execute(async (d) => provider.call(that, d, ...args), dependency, scope)
     }
+  }
+
+  dispose() {
+    this.store.clear()
   }
 }
 
@@ -76,6 +86,7 @@ const globalScope = createScope()
 
 export type Executor<Value, Dependent> = {
   resolve(scope: Scope): Promise<Value>
+  modify(executor: Executor<Dependent, unknown>): void
   readonly [x: symbol]: true
 }
 
@@ -85,10 +96,40 @@ function isExecutor<P, D>(obj: any): obj is Executor<P, D> {
   return obj?.[executorSymbol]
 }
 
+class Execution<Input extends Array<any>, Output, Dependency> {
+  constructor(
+    private executor: (dependency: Dependency, ...input: Input) => Output | Promise<Output>,
+    private dependency: Executor<Dependency, unknown>
+  ) { }
+
+  async executeIn(scope: Scope, ...input: Input): Promise<Awaited<Output>> {
+    const dependency = await this.dependency.resolve(scope)
+    return await this.executor(dependency, ...input)
+  }
+
+  async execute(...input: Input): Promise<Awaited<Output>> {
+    return await this.executeIn(getScope(), ...input)
+  }
+
+  resolve(scope: Scope = getScope()): (...input: Input) => Promise<Awaited<Output>> {
+    return this.executeIn.bind(this, scope)
+  }
+
+}
+
+export function createExecution<Dependency, Input extends Array<any>, Output>(
+  executor: (dependency: Dependency, ...input: Input) => Output | Promise<Output>,
+  dependency: Executor<Dependency, unknown>
+): Execution<Input, Output, Dependency> {
+  return new Execution(executor, dependency)
+}
+
 export function create<P, D>(providerClass: ProviderClass<P, D>): Executor<P, D>
 export function create<P>(provider: Provider<P>): Executor<P, unknown>
 export function create<P, D>(provider: Provider<P, D>, dependencies: Executor<D, unknown>): Executor<P, D>
 export function create<P, D>(provider: Provider<P, D> | ProviderClass<P, D>, dependencies?: Executor<D, unknown>): Executor<P, D> {
+  let modifiableDependency = dependencies
+
   async function resolve(scope: Scope): Promise<P> {
     if (provider instanceof ProviderClass) {
       return await provider.run(scope)
@@ -99,7 +140,7 @@ export function create<P, D>(provider: Provider<P, D> | ProviderClass<P, D>, dep
     }
 
     const promise = Promise.resolve()
-      .then(() => dependencies?.resolve(scope))
+      .then(() => modifiableDependency?.resolve(scope))
       .then(async (actualized) => {
         if (provider.length > 0 && actualized === undefined) {
           throw new Error(`invalid state, provider ${provider.toString()} requires dependent but not provided`)
@@ -110,32 +151,38 @@ export function create<P, D>(provider: Provider<P, D> | ProviderClass<P, D>, dep
       .catch((e) => {
         throw e
       })
-      
+
     scope.set(executor, promise)
     return await promise
   }
 
-  const executor: Executor<P, D> = { resolve, [Symbol.for('$submodule')]: true }
+  function modify(executor: Executor<D, unknown>) {
+    modifiableDependency = executor
+  }
+
+  const executor: Executor<P, D> = { modify, resolve, [Symbol.for('$submodule')]: true }
 
   return executor
 }
 
 export const value = <Provide>(value: Provide) => create(() => value)
 
-export const prepare = <Dependent, Input extends Array<any>, Output>(
-  provider: (provide: Dependent, ...input: Input) => Output,
+export function prepare<Dependent, Input extends Array<any>, Output>(
+  provider: ((provide: Dependent, ...input: Input) => Output),
   dependency: Executor<Dependent, unknown>,
   scope: Scope = getScope()
-): (...input: Input) => Promise<Awaited<ReturnType<typeof provider>>> => {
-  return scope.prepare(provider, dependency)
+): (...input: Input) => Promise<Awaited<Output>> {
+  const execution = new Execution(provider, dependency)
+  return execution.resolve(scope)
 }
 
 export async function execute<Dependent, Output>(
-  executable: Provider<Output, Dependent>, 
+  executable: Provider<Output, Dependent>,
   dependency: Executor<Dependent, unknown>,
   scope: Scope = getScope()
 ): Promise<Awaited<Output>> {
-  return await scope.execute(executable, dependency)
+  const execution = new Execution(executable, dependency)
+  return execution.executeIn(scope)
 }
 
 export async function resolve<T>(executor: Executor<T, any>, scope: Scope = getScope()): Promise<T> {
@@ -143,7 +190,7 @@ export async function resolve<T>(executor: Executor<T, any>, scope: Scope = getS
 }
 
 export function resolveValue<T>(
-  executor: Executor<T, any>, 
+  executor: Executor<T, any>,
   value: T | Executor<T, any>,
   scope: Scope = getScope()
 ) {
@@ -152,7 +199,9 @@ export function resolveValue<T>(
 
 export type inferProvide<T> = T extends Executor<infer S, any> ? S : never
 
-export const combine = function <L extends Record<string, Executor<any, any>>>(layout: L): Executor<{ [key in keyof L]: inferProvide<L[key]> }, unknown> {
+export function combine<
+  L extends Record<string, Executor<any, any>>
+>(layout: L): Executor<{ [key in keyof L]: inferProvide<L[key]> }, unknown> {
   return create(new ProviderClass(async (scope) => {
     const layoutPromise = Object.entries(layout).map(([key, executor]) => executor.resolve(scope).then(value => [key, value] as const))
     const result = Object.fromEntries(await Promise.all(layoutPromise))
--- pkgs/core/vite.config.mts
+++ pkgs/core/vite.config.mts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      provider: 'c8',
       reporter: ['text', 'json', 'html'],
     },
   },
